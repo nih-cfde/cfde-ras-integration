eaa76bb3bf4b50cd47613dabe99d48237684d218
Nickolaus D. Saint <nick@globus.org>
2020-10-30 08:26:47 -0700

    Removed other providers, except globus auth

--- app/settings.py
+++ app/settings.py
@@ -15,8 +15,8 @@ SOCIAL_AUTH_USER_MODEL = 'app.models.User'
 SOCIAL_AUTH_STORAGE = 'social_flask_sqlalchemy.models.FlaskStorage'
 SOCIAL_AUTH_AUTHENTICATION_BACKENDS = (
     'social_core.backends.globus.GlobusOpenIdConnect',
-    'social_core.backends.github.GithubOAuth2',
-    'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.google.GoogleOpenId',
-    'social_core.backends.slack.SlackOAuth2'
+    # 'social_core.backends.github.GithubOAuth2',
+    # 'social_core.backends.google.GoogleOAuth2',
+    # 'social_core.backends.google.GoogleOpenId',
+    # 'social_core.backends.slack.SlackOAuth2'
 )
