4026a9cd0345f275bcc087e18e9b28e63f2fcba5
Nickolaus D. Saint <nick@globus.org>
2020-11-19 13:39:54 -0800

    RAS: Passports are now verified on successful Auth

--- app/ras.py
+++ app/ras.py
@@ -24,6 +24,31 @@ class RasOpenIDConnect(OpenIdConnectAuth):
         ('ga4gh_passport_v1', 'ga4gh_passport_v1', True),
     ]
 
+    def get_user_details(self, response):
+        # Only include passports that verify
+        algorithm = self.get_algorithm(response['id_token'])
+        response['ga4gh_passport_v1'] = [
+            passport for passport in response.get('ga4gh_passport_v1', [])
+            if self.verify_jwt(passport, algorithm)
+        ]
+        return super().get_user_details(response)
+
+    def get_algorithm(self, id_token):
+        """Get the algorithm 'alg' set on an id_token header"""
+        header, _, _ = id_token.split('.')
+        return json.loads(base64url_decode(header)).get('alg')
+
+    def verify_jwt(self, jwt, algorithm):
+        """Verifies a jwt and returns the key used to do so. Returns None if
+        verification fails."""
+        message, encoded_sig = jwt.rsplit('.', 1)
+        decoded_sig = base64url_decode(encoded_sig.encode('utf-8'))
+        for key in self.get_jwks_keys():
+            key['alg'] = key.get('alg') or algorithm
+            rsakey = jwk.construct(key)
+            if rsakey.verify(message.encode('utf-8'), decoded_sig):
+                return key
+
     def find_valid_key(self, id_token):
         """
         Currently, 'alg' is not provided in the JWKS keys, which is needed by
@@ -31,16 +56,4 @@ class RasOpenIDConnect(OpenIdConnectAuth):
         field is optional for JWKS keys. The following looks for the 'alg' on
         the id_token if it is not provided in the JWKS keys.
         """
-        # Get signature
-        message, encoded_sig = id_token.rsplit('.', 1)
-        decoded_sig = base64url_decode(encoded_sig.encode('utf-8'))
-        # Get header info
-        header, _ = message.split('.')
-        header_info = json.loads(base64url_decode(header))
-        for key in self.get_jwks_keys():
-            # Fallback on the algorithm set in the id_token header if no
-            # key is specified on the JWKS key
-            key['alg'] = key.get('alg') or header_info.get('alg')
-            rsakey = jwk.construct(key)
-            if rsakey.verify(message.encode('utf-8'), decoded_sig):
-                return key
+        return self.verify_jwt(id_token, self.get_algorithm(id_token))
